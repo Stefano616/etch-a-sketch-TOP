dd842e1cc78ade7dcd3c557434767aefb5960487
Stefano Rocchetti <stefanorocchetti4@gmail.com>
2024-08-28 22:59:07 +0200

    Add hover effect to the grid elements

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function createDivGrid(size){
     for (let i = 0; i<(size*size); i++){
         const gridElement = document.createElement("div");
         gridElement.classList.add("grid-element");
-
+        
         gridContainer.appendChild(gridElement);
     }
-    
+    gridContainer.addEventListener('mouseover', (e)=>e.target.classList.add("hovered"));
     return size;
 }
 
