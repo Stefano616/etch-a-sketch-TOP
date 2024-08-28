0e9b993e04e696731d231b29766f6bf4ffd913b8
Stefano Rocchetti <stefanorocchetti4@gmail.com>
2024-08-28 23:52:49 +0200

    Add button for creating new grid with chosen size
    The button allows users to create a new grid with the desired size.
    A function for checking the validity of the new size is also implemented.
    Value greater than 100 are not allowed since may cause performance issues.

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 const gridContainer = document.querySelector(".container");
+const newGridBtn = document.querySelector("#newGridBtn");
+let gridSize = 16;
+let gridSizeIsValid = false;
 
-function createDivGrid(size){
+newGridBtn.addEventListener('click', ()=> {
+    inputGridSize = prompt('Insert the desired grid size');
+    gridSizeIsValid = checkValidGridSize(inputGridSize);
+    if (gridSizeIsValid){
+        gridSize = Number(inputGridSize);
+        createGrid(gridSize);
+        return gridSize;
+    } else {
+        alert('Please enter a valid number lower than 100 =)');
+    } 
+}
+);
+
+function checkValidGridSize(size){
+    if (size === ''|| Number(size)>100 || isNaN(size)){
+        return false;
+    }
+    else {
+        return true;
+    }
+
+}
+
+function createGrid(size){
+    gridContainer.textContent='';
     for (let i = 0; i<(size*size); i++){
         const gridElement = document.createElement("div");
+        gridElement.style.width = `${(100 / size)}%`;
         gridElement.classList.add("grid-element");
-        
         gridContainer.appendChild(gridElement);
     }
     gridContainer.addEventListener('mouseover', (e)=>e.target.classList.add("hovered"));
     return size;
 }
 
-createDivGrid(16);
+createGrid(gridSize);
